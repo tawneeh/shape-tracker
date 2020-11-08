db790ce3a60dd638fa2094810a7201c0c0874acd
Tawnee Harris <tawneeh@icloud.com>
2020-11-08 15:30:46 -0800

    add environment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-$(dpcument).ready(function() {
+import Triangle from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+$(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
     const length1 = $('#length1').val();
